80373c68ba24b114b1cef43e229610d19ced1ef4
liloudhdb <liloudehdeb@gmail.com>
2025-01-20 10:13:52 +0100

    amélioration

---
+++ script.js
@@ -0,0 +1,9 @@
+/*// Fonction pour afficher/masquer le menu
+function toggleMenu() {
+    const menu = document.getElementById("dropdown-menu");
+    if (menu.style.display === "block") {
+      menu.style.display = "none"; // Cache le menu
+    } else {
+      menu.style.display = "block"; // Affiche le menu
+    }
+  }*/
